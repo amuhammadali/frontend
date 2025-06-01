4076c3d56fea98b156207015da072beda2452033
amuhammadali <muhammadaliadhamov92@gmail.com>
2025-06-01 22:48:21 +0500

    changes deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
 // 👇 Service Worker’ni unregister qilish (cache’ni oldini olish)
 if ('serviceWorker' in navigator) {
